699b68ce49215202bd2d52fb893c81ad1acfe23e
Jess Robertson <jesse.robertson@csiro.au>
2014-06-25 07:02:34 +0800

    Add utilities functions

---
+++ pydym/utilities.py
@@ -0,0 +1,47 @@
+""" file:   utilities.py (pydym)
+    author: Jess Robertson
+            CSIRO Minerals Resources Flagship
+    date:   Tuesday 24 June 2014
+
+    description: Utility functions for pydym
+"""
+
+import sys
+
+
+class ProgressBar:
+
+    """ A progress bar class which will work in Python and IPython
+
+        Stolen from PyMC
+    """
+
+    def __init__(self, iterations):
+        self.iterations = iterations
+        self.prog_bar = '[]'
+        self.fill_char = '*'
+        self.width = 40
+        self.__update_amount(0)
+
+    def animate(self, iter):
+        print '\r', self,
+        sys.stdout.flush()
+        self.update_iteration(iter + 1)
+
+    def update_iteration(self, elapsed_iter):
+        self.__update_amount((elapsed_iter / float(self.iterations)) * 100.0)
+        self.prog_bar += r'  Running'
+
+    def __update_amount(self, new_amount):
+        percent_done = int(round((new_amount / 100.0) * 100.0))
+        all_full = self.width - 2
+        num_hashes = int(round((percent_done / 100.0) * all_full))
+        self.prog_bar = ('[' + self.fill_char * num_hashes + ' '
+                         * (all_full - num_hashes) + ']')
+        pct_place = (len(self.prog_bar) // 2) - len(str(percent_done))
+        pct_string = '%d%%' % percent_done
+        self.prog_bar = self.prog_bar[0:pct_place] + \
+            (pct_string + self.prog_bar[pct_place + len(pct_string):])
+
+    def __str__(self):
+        return str(self.prog_bar)
