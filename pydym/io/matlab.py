f636fcb2e81ad58f7f02e8ff76c035b2d72f788e
Jess Robertson <jesse.robertson@csiro.au>
2014-06-25 07:02:19 +0800

    Adding files for MATLAB IO

---
+++ pydym/io/matlab.py
@@ -0,0 +1,7 @@
+""" file:   matlab.py (pydym.io)
+    author: Jess Robertson
+            CSIRO Minerals Resources Flagship
+    date:   Tuesday 24 June 2014
+
+    description: Matlab I/O module for pydym
+"""
