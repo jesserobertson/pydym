3f758fa769187cd7a00e56ac6e0de52e57816b00
Jess Robertson <jesse.robertson@csiro.au>
2014-06-24 16:10:05 +0800

    Updating imports

---
+++ pydym/__init__.py
@@ -0,0 +1,15 @@
+""" file: __init__.py (pydym)
+    author: Jess Robertsonm
+            CSIRO Minerals Resources Flagship
+    date:   Tuesday 24 June, 2014
+
+    description: Imports for pydym
+"""
+
+from dynamic_decomposition import dynamic_decomposition
+from flow_data import FlowData, FlowDatum
+import io
+import plotting
+
+__all__ = [io, plotting, dynamic_decomposition,
+           FlowData, FlowDatum]
