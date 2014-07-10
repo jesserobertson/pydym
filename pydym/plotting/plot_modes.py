4171f88b2a6f1ae5246fd3ad9883a71d7e2f76a5
Jess Robertson <jesse.robertson@csiro.au>
2014-07-11 07:43:18 +0800

    Adding plotting for dynamic modes

---
+++ pydym/plotting/plot_modes.py
@@ -0,0 +1,12 @@
+""" file:   plot_modes.py
+    author: Jess Robertson
+            CSIRO Minerals Resources Flagship
+    date:   Friday 11 July 2014
+
+    description: Plotting dynamic mode data
+"""
+
+def plot_modes(data)
+    """ Plot the modes derived from a dynamic decomposition of the flow data.
+    """
+    pass
