268d7c9675c98b2788411199f32e09eee68025ab
Jess Robertson <jesse.robertson@csiro.au>
2014-07-11 07:44:26 +0800

    Add interpolator to plotting

--- pydym/plotting/utilities.py
+++ pydym/plotting/utilities.py
@@ -6,7 +6,10 @@
     description: Utility functions for plotting
 """
 
+from __future__ import division
+import numpy
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
 
 def make_axes_grid(nplots, ncols=3):
@@ -21,3 +24,14 @@ def make_axes_grid(nplots, ncols=3):
     if ncols * nrows < nplots:
         nrows += 1
     return plt.GridSpec(nrows, ncols)
+
+
+def interpolator(data):
+    """ Generate an interpolator for the flow data
+    """
+    xval, yval = data['position/x'], data['position/y']
+    xlim = xval.min(), xval.max()
+    ylim = yval.min(), yval.max()
+    nx, ny = map(len, (xval, yval))
+    xs, ys = numpy.linspace(*xlim, num=nx), numpy.linspace(*ylim, num=ny)
+    return lambda var: mlab.griddata(xval, yval, xs, ys, interp='linear')
