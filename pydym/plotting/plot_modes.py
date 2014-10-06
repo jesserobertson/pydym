e680a220bc1bde419cf4c94be50c204eb4e0b037
Jess Robertson <jesse.robertson@csiro.au>
2014-10-06 15:12:18 +0800

    Fixing plotting modes function

--- pydym/plotting/plot_modes.py
+++ pydym/plotting/plot_modes.py
@@ -8,20 +8,24 @@
 
 from .utilities import interpolator
 
+import numpy
+import matplotlib.pyplot as plt
+
 
 def plot_modes(data, axes=None, n_quiver=3, n_contours=20):
     """ Plot the modes derived from a dynamic decomposition of the flow data.
     """
     xs, ys, interp = interpolator(data)
-
-    abs_velocity = data.velocity[:, ]
+    abs_velocity = numpy.sqrt(
+        data.velocity[::n_quiver, 0] ** 2
+        + data.velocity[::n_quiver, 1] ** 2)
 
     # Plot the results
     if axes is None:
         plt.figure(figsize=(11, 11))
         axes = plt.gca()
     axes.set_aspect('equal')
-    axes.contourf(xs, ys, Ps, n_contours,
+    axes.contourf(xs, ys, abs_velocity, n_contours,
                   cmap=plt.get_cmap('RdYlBu'),
                   alpha=0.6)
     axes.quiver(data.position[::n_quiver, 0],
@@ -36,4 +40,4 @@ def plot_modes(data, axes=None, n_quiver=3, n_contours=20):
                    facecolor='white'))
     axes.set_axis_off()
     axes.set_xlim(min(xs), max(xs))
-    axes.set_ylim(min(ys), max(ys
+    axes.set_ylim(min(ys), max(ys))
