0419c44c247e4aef7d3c186f0c2d87019e0ce40c
Jess Robertson <jesse.robertson@csiro.au>
2015-02-10 07:05:42 +0800

    Adding versioning

--- setup.py
+++ setup.py
@@ -10,23 +10,23 @@
 
 from setuptools import setup, find_packages
 
+# Get requirements from requirements.txt file
+with open('requirements.txt') as fhandle:
+    REQUIREMENTS = [l.strip('\n') for l in fhandle]
+
+# Get version number from _version.py
+# Can be updated using python setup.py update_version
+from update_version import update_version, Version, get_version
+update_version()
 
 ## PACKAGE INFORMATION
 setup(
+    # Metadata
     name='pydym',
     version='0.0.1',
     description='Dynamic mode decompositions of data in Python',
     author='Jess Robertson',
     author_email='jesse.robertson@csiro.au',
-    packages=find_packages(exclude=['tests', 'docs']),
-    install_requires=[
-        'h5py>=2.0.0',
-        'matplotlib>=1.0.0',
-        'numpy>=1.8.0',
-        'scipy>=0.10.0',
-    ],
-    test_suite='tests',
-    ext_modules=[],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Environment :: Plugins',
@@ -45,5 +45,16 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7'
-    ]
+    ],
+
+    # Requirements
+    install_requires=REQUIREMENTS,
+
+    # Contents
+    packages=find_packages(exclude=['tests', 'docs']),
+    test_suite='tests',
+    ext_modules=[],
+    cmdclass={
+        'update_version': Version,
+    }
 )
---
+++ update_version.py
@@ -0,0 +1,90 @@
+""" file: update_version.py
+    author: Jess Robertson
+            CSIRO Minerals Resources Flagship
+    date:   January 2015
+
+    description: Tool to update the version number using `git describe`
+"""
+
+import subprocess
+import os
+from setuptools import Command
+import re
+import logging
+
+LOGGER = logging.getLogger()
+
+VERSION_PY_TEMPLATE = """\
+# This file is originally generated from Git information by running 'setup.py
+# update_version'. Distribution tarballs contain a pre-generated copy of this
+# file.
+__version__ = '{0}'
+"""
+
+
+def update_version():
+    # Query git for the current description
+    if not os.path.isdir(".git"):
+        print ("This does not appear to be a Git repository, leaving "
+                    "pydym/_version.py alone.")
+        return
+    try:
+        p = subprocess.Popen(["git", "describe", "--always"],
+                             stdout=subprocess.PIPE)
+        stdout = p.communicate()[0]
+        if p.returncode != 0:
+            raise EnvironmentError
+        else:
+            ver = stdout.strip().split('-')
+            if len(ver) > 1:
+                ver = ver[0] + '.dev' + ver[1]
+    except EnvironmentError:
+        print (
+            "Unable to run git, leaving pydym/_version.py alone")
+        return
+
+    # Write to file
+    current_ver = get_version()
+    if current_ver != ver:
+        print ("Version {0} out of date, updating to {1}".format(
+            current_ver, ver))
+        with open('pydym/_version.py', 'wb') as fhandle:
+            fhandle.write(VERSION_PY_TEMPLATE.format(ver))
+
+
+def get_version():
+    """ Get the currently set version
+    """
+    try:
+        with open("pydym/_version.py") as fhandle:
+            for line in (f for f in fhandle if not f.startswith('#')):
+                return re.match("__version__ = '([^']+)'", line).group(1)
+    except EnvironmentError:
+        LOGGER.error(
+            "Can't find pydym/_version.py - what's the version, doc?")
+        return 'unknown'
+
+
+class Version(Command):
+
+    """ Setuptools command to update version
+
+        to execute run python setup.py version
+    """
+
+    description = "update _version.py from Git repo"
+    user_options = []
+    boolean_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        update_version()
+        print ("Version is now {0}".format(get_version()))
+
+if __name__ == '__main__':
+    update_version()
