c1f879f5366806b5f24c983cb6a835bd701575ac
Jess Robertson <jesse.robertson@csiro.au>
2015-02-12 19:10:56 +0800

    Add eigenplot

--- pydym/plotting/eigenplots.py
+++ pydym/plotting/eigenplots.py
@@ -7,17 +7,79 @@
 """
 
 import matplotlib.pyplot as plt
-import numpy
+from numpy import sin, cos, pi, linspace, log
 
 
-def eigenplot(array):
-    """ Plot complex eigenvalues from a matrix
+def eigenplot(eigenvalues, amplitudes=None, cmap=None):
+    """ Generate an eigenvalue plot with optional shading
+
+        Parameters:
+            eigenvalues - the eigenvalues to plot
+            amplitudes - the amplitudes of each eigenvalue. Optional, if None
+                then all eigenvalues are plotted in black, if specified then
+                the amplitudes are used with the colormap
+            cmap - the name of the matplotlib colormap to use
+
+        Returns:
+            (fig, axes) - Handles to the matplotlib.pyplot.figure and .axes
+                instances containing the plot.
     """
-    eigenvalues, eigenvectors = numpy.eig(array)
-    pass
+    # Pull out relevant info
+    eigvals = dmd_result['eigenvalues']
+    alpha = dmd_result['amplitudes']
+
+    # Set up background colors etc
+    ts = linspace(0, 2*pi, 299)
+    circle = sin(ts), cos(ts)
+    if amplitudes:
+        cmap = plt.get_cmap(cmap if cmap else 'coolwarm')
+        shade_by = log(abs(alpha))
+        colors = cmap((shade_by - shade_by.min())
+                      / (shade_by.max() - shade_by.min()))
+
+    # Generate two plots, one with all values, one zoomed in on 1 + 0j
+    fig = plt.figure(figsize=(8, 4))
+    for axidx in (1, 2):
+        axes = plt.subplot(1, 2, axidx)
+        if colored:
+            axes.scatter(eigvals.real, eigvals.imag,
+                         marker='o', edgecolor=colors, facecolor=colors)
+        else:
+            axes.scatter(eigvals.real, eigvals.imag,
+                         marker='.', edgecolor='black', facecolor='black')
+        axes.plot(*circle, color='black', dashes=(5, 3))
+        axes.set_aspect('equal')
+        axes.set_xlim(-1.01, 1.01)
+        axes.set_ylim(-1.01, 1.01)
+        axes.set_xlabel(r'Re($\lambda_{i}$)')
+        axes.set_ylabel(r'Im($\lambda_{i}$)')
+
+    # Resize second set of axes
+    axes.set_aspect(0.1 / 0.4)
+    axes.set_xlim(0.8, 1)
+    axes.set_ylim(-0.5, 0.5)
+    fig.tight_layout()
+    return fig, axes
 
 
-def logeigenplot(array):
+def logeigenplot(eigenvalues, amplitudes=None, cmap=None):
     """ Plot log-transformed eigenvalues from a matrix
     """
     pass
+
+
+def spy(array):
+    """ Plot the log values of an array
+
+        Parameters
+            array - the array to plot
+
+        Returns
+            (fig, axes) - Handles to the figure and axes containing the plot
+    """
+    plt.figure(figsize=(11, 11))
+    axes = plt.gca()
+    axes.imshow(log(array), interpolation='none')
+    axes.set_axis_off()
+    axes.set_aspect('equal')
+    return fig, axes
