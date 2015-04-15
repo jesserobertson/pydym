87416bd142ee51c19da297820c5493f65e0947c8
Jess Robertson <jesse.robertson@csiro.au>
2015-04-15 09:12:30 +0800

    Making sure test data is generateable

---
+++ tests/resources/run_parameters.py
@@ -0,0 +1,15 @@
+PARAMETERS = {
+    'cavity_aspect':           1,
+    'density_ratio':           1,
+    'end_time':                300.0,
+    'gerris':                  'gerris2D',
+    'gfsfile':                 '/media/rayleigh/chaos_runs/chaos.gfs',
+    'num_cycles':              30,
+    'num_processors':          8,
+    'num_split':               2,
+    'reynolds_number':         64,
+    'run_name':                'chaos-1.0.64.10',
+    'simulation_output_times': 0.5,
+    'strouhal_number':         0.1,
+    'viscosity_ratio':         1
+}
