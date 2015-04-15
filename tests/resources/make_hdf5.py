682bfebc82a28c30bc7e2847efafe855f19524e8
Jess Robertson <jesse.robertson@csiro.au>
2015-04-15 09:11:52 +0800

    Fixing make HDF5 stuff

--- tests/resources/make_hdf5.py
+++ tests/resources/make_hdf5.py
@@ -10,11 +10,15 @@ from __future__ import division, print_function
 from pydym.io.gerris import GerrisReader
 import shutil
 
+from run_parameters import PARAMETERS
+
 def main():
     reader = GerrisReader('vertices.csv')
     flow_data = reader.process_directory('simulations',
                                          update=True,
-                                         clean=False)
+                                         clean=False,
+                                         show_progress=True,
+                                         run_parameters=PARAMETERS)
     shutil.move('simulations/simulations.hdf5', 'simulations.hdf5')
 
 if __name__ == '__main__':
